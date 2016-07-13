40032ff945434a7a83335b326b5634f02b6a014b
fabienthetis <fabien2.thetis@gmail.com>
2016-07-13 11:06:13 +0200

    update collection name mongodb

--- config/connections.js
+++ config/connections.js
@@ -62,7 +62,7 @@ module.exports.connections = {
     port: 27017,
     // user: 'username',
     // password: 'password',
-    database: 'open-shop'
+    database: 'ymple-commerce'
   },
 
   /***************************************************************************
