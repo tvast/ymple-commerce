03d48af87fb5f4372ea83501fe4326a52d3c8e62
fabienthetis <fabien2.thetis@gmail.com>
2016-07-18 12:34:13 +0200

    update route

--- config/routes.js
+++ config/routes.js
@@ -22,6 +22,6 @@
 
 
 
-var route_front_end = require('./route_front_end');
+var route_front_end = require('./route_front');
 var route_api = require('./route_api');
-var route_back_end = require('./route_back_end');
+var route_back_end = require('./route_admin');
